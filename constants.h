263a2effb03fb9216af7413dc1d162ebecd32de4
Kiriaki Pantelidou <knpantelidou@gmail.com>
2020-04-11 20:26:56 +0300

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,11 @@
+#ifndef TRIANGLES_CONSTANTS_H
+#define TRIANGLES_CONSTANTS_H
+
+#define FILENAME        "CA-AstroPh-new.txt"
+#define DELIMETER       " "
+#define CHUNK_PREFIX    "_chunk_"
+#define TAG_DATA        0
+#define TAG_NOTIFIER    1
+
+
+#endif //TRIANGLES_CONSTANTS_H
